e12ff2a307d06d36f66d61199e705e9a15ff78f1
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-06-18 19:41:10 +0500

    feat: handle single invoice getter

---
+++ internal/dto/invoiceDto/getInvoiceDto.go
@@ -0,0 +1,77 @@
+package invoiceDto
+
+import (
+	"invoice-dashboard/internal/entity"
+	"time"
+)
+
+type GetAddressDto struct {
+	Street   string `json:"street"`
+	City     string `json:"city"`
+	PostCode string `json:"postCode"`
+	Country  string `json:"country"`
+}
+
+type GetItemDto struct {
+	ID       uint64  `json:"id"`
+	Name     string  `json:"name"`
+	Quantity int     `json:"quantity"`
+	Price    float32 `json:"price"`
+	Total    float32 `json:"total"`
+}
+
+type InvoiceResponse struct {
+	ID            uint64        `json:"id"`
+	PaymentDue    time.Time     `json:"paymentDue"`
+	Description   string        `json:"description"`
+	PaymentTerms  int           `json:"paymentTerms"`
+	ClientName    string        `json:"clientName"`
+	ClientEmail   string        `json:"clientEmail"`
+	Status        string        `json:"status"`
+	SenderAddress GetAddressDto `json:"senderAddress"`
+	ClientAddress GetAddressDto `json:"clientAddress"`
+	Items         []GetItemDto  `json:"items"`
+	Total         float32       `json:"total"`
+	CreatedAt     time.Time     `json:"createdAt"`
+	UpdatedAt     time.Time     `json:"updatedAt"`
+}
+
+func EntitytoResponsetDTO(invoice *entity.Invoice) InvoiceResponse {
+	var items []GetItemDto = make([]GetItemDto, len(invoice.Items))
+
+	for i, item := range invoice.Items {
+		items[i] = GetItemDto{
+			ID:       item.ID,
+			Name:     item.Name,
+			Quantity: item.Quantity,
+			Price:    item.Price,
+			Total:    item.Total,
+		}
+	}
+
+	getInvoiceDto := InvoiceResponse{
+		ID:           invoice.ID,
+		PaymentDue:   invoice.PaymentDue,
+		Description:  invoice.Description,
+		PaymentTerms: invoice.PaymentTerms,
+		ClientName:   invoice.ClientName,
+		ClientEmail:  invoice.ClientEmail,
+		Status:       invoice.Status,
+		SenderAddress: GetAddressDto{
+			Street:   invoice.SenderAddress.Street,
+			City:     invoice.SenderAddress.City,
+			PostCode: invoice.SenderAddress.PostCode,
+			Country:  invoice.SenderAddress.Country,
+		},
+		ClientAddress: GetAddressDto{
+			Street:   invoice.ClientAddress.Street,
+			City:     invoice.ClientAddress.City,
+			PostCode: invoice.ClientAddress.PostCode,
+			Country:  invoice.ClientAddress.Country,
+		},
+		Items: items,
+		Total: invoice.Total,
+	}
+
+	return getInvoiceDto
+}
--- internal/invoice/service.go
+++ internal/invoice/service.go
@@ -2,6 +2,7 @@ package invoice
 
 import (
 	"invoice-dashboard/internal/dto/invoiceDto"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,7 +30,24 @@ func GetAll(context *gin.Context) {
 	context.JSON(200, invoices)
 }
 
-func GetById(context *gin.Context) {}
+func GetById(context *gin.Context) {
+	invoiceId := context.Param("invoiceId")
+
+	id, err := strconv.ParseUint(invoiceId, 10, 64)
+	if err != nil {
+		context.AbortWithStatusJSON(400, map[string]string{
+			"status":  "400",
+			"message": "id is not valid",
+		})
+		return
+	}
+
+	invoice := FindInvoiceById(id)
+	// need to check for 404
+	invoiceDto := invoiceDto.EntitytoResponsetDTO(&invoice)
+
+	context.JSON(200, invoiceDto)
+}
 
 func EditInvoice(context *gin.Context) {}
 
