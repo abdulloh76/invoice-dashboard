3b8a4c5f256e3abb99a2a8f39ecbd679fffe9cd0
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2022-10-14 11:15:23 +0500

    refactor: util functions to separate pkg

--- pkg/handlers/apigateway.go
+++ pkg/handlers/apigateway.go
@@ -2,8 +2,8 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
+	"github.com/abdulloh76/invoice-dashboard/pkg/utils"
 	"net/http"
 	"strings"
 
@@ -28,136 +28,104 @@ func NewAPIGatewayHandler(d *domain.Invoices, userGrpcClient *infrastructure.Use
 func (g *APIGatewayHandler) AllHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	allInvoices, err := g.invoices.AllInvoices()
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
-	return response(http.StatusOK, allInvoices), nil
+	return utils.Response(http.StatusOK, allInvoices), nil
 }
 
 func (g *APIGatewayHandler) GetHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	id, ok := event.PathParameters["id"]
 	if !ok {
-		return errResponse(http.StatusBadRequest, "missing 'id' parameter in path"), nil
+		return utils.ErrResponse(http.StatusBadRequest, "missing 'id' parameter in path"), nil
 	}
 
 	invoice, err := g.invoices.GetSingleInvoice(id)
 
-	if errors.Is(err, domain.ErrInvoiceNotFound) {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+	if errors.Is(err, utils.ErrInvoiceNotFound) {
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
-	// todo will be updated when gRPC client implemented
+	// todo will be updated when getting user_id configured
 	senderAddress, err := g.userGrpcClient.GetUserAddress(ctx, "mHVxHT4VR")
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
 	invoiceDto := types.EntityToResponseDTO(invoice, senderAddress)
-	return response(http.StatusOK, invoiceDto), nil
+	return utils.Response(http.StatusOK, invoiceDto), nil
 }
 
 func (g *APIGatewayHandler) CreateHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	if strings.TrimSpace(event.Body) == "" {
-		return errResponse(http.StatusBadRequest, "empty request body"), nil
+		return utils.ErrResponse(http.StatusBadRequest, "empty request body"), nil
 	}
 
 	newInvoice, err := g.invoices.Create([]byte(event.Body))
-	if errors.Is(err, domain.ErrJsonUnmarshal) {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+	if errors.Is(err, utils.ErrJsonUnmarshal) {
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
-	// todo will be updated when gRPC client implemented
+	// todo will be updated when getting user_id configured
 	senderAddress, err := g.userGrpcClient.GetUserAddress(ctx, "mHVxHT4VR")
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
 	invoiceDto := types.EntityToResponseDTO(newInvoice, senderAddress)
-	return response(http.StatusOK, invoiceDto), nil
+	return utils.Response(http.StatusOK, invoiceDto), nil
 }
 
 func (g *APIGatewayHandler) PutHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	id, ok := event.PathParameters["id"]
 	if !ok {
-		return errResponse(http.StatusBadRequest, "missing 'id' parameter in path"), nil
+		return utils.ErrResponse(http.StatusBadRequest, "missing 'id' parameter in path"), nil
 	}
 
 	if strings.TrimSpace(event.Body) == "" {
-		return errResponse(http.StatusBadRequest, "empty request body"), nil
+		return utils.ErrResponse(http.StatusBadRequest, "empty request body"), nil
 	}
 
 	updatedInvoice, err := g.invoices.ModifyInvoice(id, []byte(event.Body))
-	if errors.Is(err, domain.ErrInvoiceNotFound) {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+	if errors.Is(err, utils.ErrInvoiceNotFound) {
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
-	if errors.Is(err, domain.ErrJsonUnmarshal) {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+	if errors.Is(err, utils.ErrJsonUnmarshal) {
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
-	// todo will be updated when gRPC client implemented
+	// todo will be updated when getting user_id configured
 	senderAddress, err := g.userGrpcClient.GetUserAddress(ctx, "mHVxHT4VR")
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
 	invoice := types.EntityToResponseDTO(updatedInvoice, senderAddress)
-	return response(http.StatusOK, invoice), nil
+	return utils.Response(http.StatusOK, invoice), nil
 }
 
 func (g *APIGatewayHandler) DeleteHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	id, ok := event.PathParameters["id"]
 	if !ok {
-		return errResponse(http.StatusBadRequest, "missing 'id' parameter in path"), nil
+		return utils.ErrResponse(http.StatusBadRequest, "missing 'id' parameter in path"), nil
 	}
 
 	err := g.invoices.DeleteInvoice(id)
-	if errors.Is(err, domain.ErrInvoiceNotFound) {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+	if errors.Is(err, utils.ErrInvoiceNotFound) {
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error()), nil
+		return utils.ErrResponse(http.StatusInternalServerError, err.Error()), nil
 	}
 
-	return response(http.StatusOK, nil), nil
-}
-
-func response(code int, object interface{}) events.APIGatewayProxyResponse {
-	marshalled, err := json.Marshal(object)
-	if err != nil {
-		return errResponse(http.StatusInternalServerError, err.Error())
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: code,
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Body:            string(marshalled),
-		IsBase64Encoded: false,
-	}
-}
-
-func errResponse(status int, body string) events.APIGatewayProxyResponse {
-	message := map[string]string{
-		"message": body,
-	}
-
-	messageBytes, _ := json.Marshal(&message)
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: status,
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Body: string(messageBytes),
-	}
+	return utils.Response(http.StatusOK, nil), nil
 }
